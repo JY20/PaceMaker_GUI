846fa3d4a5d47c15b4bc463ef0cfd1718cab52c0
JY20 <43075399+JY20@users.noreply.github.com>
2023-09-25 16:32:58 -0400

    removing cache files and update couple layouts (#8)
    * removing cache files and update couple layouts
    
    * adding the variables for users parameters

--- User.py
+++ User.py
@@ -2,12 +2,16 @@ import hashlib
 
 
 class User:
-    def __init__(self, name, password):
+    def __init__(self, name, password, parameters):
         self.name = name
         self.password = password
+        self.parameters = parameters
 
     def checkCredential(self, name, password):
         if(self.name == name and hashlib.sha256(password.encode('utf-8')).hexdigest() == self.password):
             return True
         else:
             return False
+
+    def updateParameters(self, newParameters):
+        return None

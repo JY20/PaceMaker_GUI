6d0e96dfccc0abd31c693525d2e0288f261d6e22
JY20 <43075399+JY20@users.noreply.github.com>
2023-10-08 16:33:05 -0400

    adding visualization for connection and device (#16)

--- src/User.py
+++ src/User.py
@@ -2,34 +2,43 @@ import hashlib
 
 
 class User:
+
+    # constructor for user
     def __init__(self, name, password, parameters, mode):
         self.name = name
         self.password = password
         self.parameters = parameters
         self.mode = mode
 
+    # checks the credentials of the user
     def checkCredential(self, name, password):
         if(self.name == name and hashlib.sha256(password.encode('utf-8')).hexdigest() == self.password):
             return True
         else:
             return False
 
+    # get user's parameter dict
     def getParameters(self):
         return self.parameters.copy()
 
+    # update user's parameter dict
     def updateParameters(self, newParameters):
         for parameter in newParameters:
             self.parameters[parameter] = newParameters[parameter]
+    # get user's name
 
     def getName(self):
         return self.name
+    # get user's mode
 
     def getMode(self):
         return self.mode
+    # set user's mode
 
     def setMode(self, mode):
         self.mode = mode
 
+    # get json format of user variables
     def getJson(self):
         parameterNames = ['Lower Rate Limit', 'Upper Rate Limit', 'Maximum Sensor Rate', 'Activity Threshold', 'Reaction Time', 'Response Factor', 'Recovery Time',
                           'Atrial Amplitude', 'Atrial Pulse Width', 'Ventricular Amplitude', 'Ventricular Pulse Width',
--- src/main.py
+++ src/main.py
@@ -6,32 +6,26 @@ import hashlib
 from User import User
 from parameterUtility import parameterUtility
 
-maxUsers = 10
+maxUsers = 10  # max users
 
-users = {}
-logging = False
-state = "login"
-mode = ["AOOR", "AAIR", "VOOR", "VVIR"]
-curMode = ""
-windowMode = "none"
-infoMessage = ""
-dataBaseFile = "./database/database.json"
-curUser = ""
+users = {}  # user dict
+logging = False  # logging for print statement for debugging
+state = "login"  # state of GUI
+curMode = ""  # mode for the user
+windowMode = "none"  # mode from events of the winodw GUI
+infoMessage = ""  # info message for users
+dataBaseFile = "./database/database.json"  # name and path to database
+curUser = ""  # current user name
+parameterUtil = parameterUtility()  # utility class object for parameter functions
+mode = ["AOOR", "AAIR", "VOOR", "VVIR"]  # list of modes
 
-parameterNamesCommon = ['Lower Rate Limit', 'Upper Rate Limit', 'Maximum Sensor Rate',
-                        'Activity Threshold', 'Reaction Time', 'Response Factor', 'Recovery Time']
-parameterNamesA = ['Atrial Amplitude', 'Atrial Pulse Width']
-parameterNamesV = ['Ventricular Amplitude', 'Ventricular Pulse Width']
-parameterNamesAIR = ['Atrial Sensitivity', 'ARP',
-                     'PVARP', 'Hysteresis', 'Rate Smoothing']
-parameterNamesVIR = ['Ventricular Sensitivity',
-                     'VRP', 'Hysteresis', 'Rate Smoothing']
-
-parameterUtil = parameterUtility()
+# remove the spaces and get the real value
 
 
 def getRealValue(value):
     return value.replace(" ", "").replace("\n", "")
+
+# create the user in database
 
 
 def createUserDB(name, password):
@@ -57,12 +51,16 @@ def createUserDB(name, password):
     else:
         return False
 
+# get all the json values of current users
+
 
 def getAllUsersCurrentJson():
     value = {}
     for user in users:
         value[user] = users[user].getJson()
     return value
+
+# get all users in database
 
 
 def getAllUsers():
@@ -73,6 +71,8 @@ def getAllUsers():
                            ['password'], data[user]['parameters'], data[user]['mode'])
     f.close()
 
+# update the database with all the users information
+
 
 def updateDatabase():
     f = open(dataBaseFile, "w")
@@ -80,13 +80,16 @@ def updateDatabase():
     f.close()
 
 
-def convertStrToInt(value):
-    if(value == ""):
-        return 0
-    return int(value)
-
-
+# convert event values to parameters dicts
 def getUpdatedParameters(values):
+    parameterNamesCommon = ['Lower Rate Limit', 'Upper Rate Limit', 'Maximum Sensor Rate',
+                            'Activity Threshold', 'Reaction Time', 'Response Factor', 'Recovery Time']
+    parameterNamesA = ['Atrial Amplitude', 'Atrial Pulse Width']
+    parameterNamesV = ['Ventricular Amplitude', 'Ventricular Pulse Width']
+    parameterNamesAIR = ['Atrial Sensitivity', 'ARP',
+                         'PVARP', 'Hysteresis', 'Rate Smoothing']
+    parameterNamesVIR = ['Ventricular Sensitivity',
+                         'VRP', 'Hysteresis', 'Rate Smoothing']
     count = 0
     updated_parameters = {}
     for parameter in parameterNamesCommon:
@@ -114,7 +117,14 @@ def getUpdatedParameters(values):
     return updated_parameters
 
 
+# get the window layout by state
 def getWindowByState():
+    connection = [('\u2B24' + ' Disconnect', 'red'),
+                  ('\u2B24' + ' Connect', 'green')]
+    device = [('\u2B24' + ' Different Device Approached', 'yellow'),
+              ('\u2B24' + ' Same Device Approached', 'green')]
+    connection_state = 0
+    device_state = 1
     sizeText = 10
     sizeInput = 30
     layoutLogin = [[sg.Text(infoMessage, text_color='red'), sg.Text()],
@@ -137,65 +147,68 @@ def getWindowByState():
         [sg.Text('', size=(sizeText, 1)), sg.Text()],
         [sg.Button('Create New User')], [sg.Button('Back to Login')]]
 
-    sizeText = 20
-    layoutHeader = [
-        [sg.Text(infoMessage, size=(50, 3),  text_color='red'), sg.Text()],
-        [sg.Text("Mode", size=(sizeText, 1)), sg.Combo(mode, size=(
-            sizeText, 1), enable_events=True, key='mode')],
-        [sg.Text('', size=(sizeText, 1)), sg.Text()],
-    ]
-
-    sizeText = 20
+    sizeText = 30
+    sizeText2 = 15
     if (state == "control"):
+        layoutHeader = [
+            [sg.Text(infoMessage, size=(50, 3),  text_color='red'), sg.Text()],
+            [sg.Text(device[device_state][0], size=(sizeText, 1), text_color=device[device_state][1]), sg.Text(connection[connection_state][0], size=(sizeText2, 1),
+                                                                                                               text_color=connection[connection_state][1])],
+            [sg.Text('Current Mode', size=(sizeText, 1)),
+             sg.Text(curMode, size=(sizeText2, 1))],
+            [sg.Text("Mode", size=(sizeText, 1)), sg.Combo(mode, size=(
+                sizeText2, 1), enable_events=True, key='mode')],
+            [sg.Text('', size=(sizeText, 1)), sg.Text()],
+        ]
         parameters = users[curUser].getParameters()
         parameterValues = parameterUtil.getParameterRangeValues()
         layoutCommonParameters = [
             [sg.Text('Lower Rate Limit (ppm)', size=(sizeText, 1)),
-             sg.Spin(parameterValues['Lower Rate Limit'], initial_value=parameters['Lower Rate Limit'], readonly=False,  size=sizeText)],
+             sg.Spin(parameterValues['Lower Rate Limit'], initial_value=parameters['Lower Rate Limit'], readonly=False,  size=sizeText2)],
             [sg.Text('Upper Rate Limit (ppm)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Upper Rate Limit'], initial_value=parameters['Upper Rate Limit'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Upper Rate Limit'], initial_value=parameters['Upper Rate Limit'], readonly=False,  size=sizeText2)],
             [sg.Text('Maximum Sensor Rate (ppm)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Maximum Sensor Rate'], initial_value=parameters['Maximum Sensor Rate'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Maximum Sensor Rate'], initial_value=parameters['Maximum Sensor Rate'], readonly=False,  size=sizeText2)],
             [sg.Text('Activity Threshold', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Activity Threshold'], initial_value=parameters['Activity Threshold'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Activity Threshold'], initial_value=parameters['Activity Threshold'], readonly=False,  size=sizeText2)],
             [sg.Text('Reaction Time (sec)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Reaction Time'], initial_value=parameters['Reaction Time'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Reaction Time'], initial_value=parameters['Reaction Time'], readonly=False,  size=sizeText2)],
             [sg.Text('Response Factor', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Response Factor'], initial_value=parameters['Response Factor'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Response Factor'], initial_value=parameters['Response Factor'], readonly=False,  size=sizeText2)],
             [sg.Text('Recovery Time (min)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Recovery Time'], initial_value=parameters['Recovery Time'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Recovery Time'], initial_value=parameters['Recovery Time'], readonly=False,  size=sizeText2)],
         ]
         layoutA = [
             [sg.Text('Atrial Amplitude', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Atrial Amplitude'], initial_value=parameters['Atrial Amplitude'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Atrial Amplitude'], initial_value=parameters['Atrial Amplitude'], readonly=False,  size=sizeText2)],
             [sg.Text('Atrial Pulse Width (ms)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Atrial Pulse Width'], initial_value=parameters['Atrial Pulse Width'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Atrial Pulse Width'], initial_value=parameters['Atrial Pulse Width'], readonly=False,  size=sizeText2)],
         ]
         layoutV = [
             [sg.Text('Ventricular Amplitude', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Ventricular Amplitude'], initial_value=parameters['Ventricular Amplitude'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Ventricular Amplitude'], initial_value=parameters['Ventricular Amplitude'], readonly=False,  size=sizeText2)],
             [sg.Text('Ventricular Pulse Width (ms)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Ventricular Pulse Width'], initial_value=parameters['Ventricular Pulse Width'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Ventricular Pulse Width'], initial_value=parameters['Ventricular Pulse Width'], readonly=False,  size=sizeText2)],
         ]
         layoutAIR = [
             [sg.Text('Atrial Sensitivity (mV)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Atrial Sensitivity'], initial_value=parameters['Atrial Sensitivity'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Atrial Sensitivity'], initial_value=parameters['Atrial Sensitivity'], readonly=False,  size=sizeText2)],
             [sg.Text('ARP (ms)', size=(sizeText, 1)), sg.Spin(
-                values=parameterValues['ARP'], initial_value=parameters['ARP'], readonly=False,  size=sizeText)],
+                values=parameterValues['ARP'], initial_value=parameters['ARP'], readonly=False,  size=sizeText2)],
             [sg.Text('PVARP (ms)', size=(sizeText, 1)), sg.Spin(
-                values=parameterValues['PVARP'], initial_value=parameters['PVARP'], readonly=False,  size=sizeText)],
+                values=parameterValues['PVARP'], initial_value=parameters['PVARP'], readonly=False,  size=sizeText2)],
             [sg.Text('Hysteresis (ppm)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Hysteresis'], initial_value=parameters['Hysteresis'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Hysteresis'], initial_value=parameters['Hysteresis'], readonly=False,  size=sizeText2)],
             [sg.Text('Rate Smoothing (%)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Rate Smoothing'], initial_value=parameters['Rate Smoothing'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Rate Smoothing'], initial_value=parameters['Rate Smoothing'], readonly=False,  size=sizeText2)],
         ]
         layoutVIR = [
             [sg.Text('Ventricular Sensitivity (mV)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Ventricular Sensitivity'], initial_value=parameters['Ventricular Sensitivity'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Ventricular Sensitivity'], initial_value=parameters['Ventricular Sensitivity'], readonly=False,  size=sizeText2)],
             [sg.Text('VRP (ms)', size=(sizeText, 1)), sg.Spin(
-                values=parameterValues['VRP'], initial_value=parameters['VRP'], readonly=False,  size=sizeText)],
+                values=parameterValues['VRP'], initial_value=parameters['VRP'], readonly=False,  size=sizeText2)],
             [sg.Text('Hysteresis (ppm)', size=(sizeText, 1)),
-             sg.Spin(values=parameterValues['Hysteresis'], initial_value=parameters['Hysteresis'], readonly=False,  size=sizeText)],
+             sg.Spin(values=parameterValues['Hysteresis'], initial_value=parameters['Hysteresis'], readonly=False,  size=sizeText2)],
             [sg.Text('Rate Smoothing (%)', size=(sizeText, 1)),
              sg.Spin(values=parameterValues['Rate Smoothing'], initial_value=parameters['Rate Smoothing'], readonly=False,  size=sizeText)],
         ]
@@ -259,6 +272,7 @@ def getWindowByState():
         return window
 
 
+# main function to run GUI
 if __name__ == '__main__':
     try:
         sg.theme('LightGrey1')
@@ -285,6 +299,7 @@ if __name__ == '__main__':
                         curUser = getRealValue(values[0])
                         infoMessage = "Welcome to Control Panel for: " + curUser
                         windowMode = users[getRealValue(values[0])].getMode()
+                        curMode = users[getRealValue(values[0])].getMode()
                     else:
                         infoMessage = "Password Incorrect or this user does not exist"
                 elif (event == "Create New User"):
